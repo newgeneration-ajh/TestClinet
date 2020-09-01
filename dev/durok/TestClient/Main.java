51f1846a6639b1234f6da5938e71d97993756297
jihunahn <angh5080@gmail.com>
2020-09-02 00:22:11 +0900

    First Commit

---
+++ dev/durok/TestClient/Main.java
@@ -0,0 +1,9 @@
+package dev.durok.TestClient;
+
+import java.net.Socket;
+
+public class Main {
+    public static void main ( String[] args ) {
+        System.out.println("Hello World");
+    }
+}
